669851205e4a345604bbcb60f2d644d28694adee
Jace Bennett <jace.bennett@gmail.com>
2013-06-05 17:46:43 -0400

    Added failing test

--- Args.Tests/SimpleModelConventionsTest.cs
+++ Args.Tests/SimpleModelConventionsTest.cs
@@ -145,5 +145,36 @@ namespace Args.Tests
             (member.TypeConverter as ArgsTypeConverter).Converter.GetType().Should().Be.EqualTo<SimpleConverter>();
             member.HelpText.Should().Be.Null();
         }
+
+
+        public class SimpleModelForWeirdnessTest {
+          public Guid? Id { get; set; }
+          public string Name { get; set; }
+        }
+
+        [Test]
+        public void TestForParsingWierdnessPasses()
+        {
+          var nameValue = "foo";
+          var idValue = "c304cffd-f331-49a1-a4ba-0706f869de11";
+
+          var modelBindingDefinition = Configuration.Configure<SimpleModelForWeirdnessTest>();
+          var model = modelBindingDefinition.CreateAndBind(new string[] {"/N", nameValue, "/I", idValue});
+
+          model.Name.Should().Be(nameValue);
+          model.Id.ToString().Should().Be(idValue);
+        }
+
+        [Test]
+        public void TestForParsingWierdnessFails() {
+          var nameValue = "foo";
+          var idValue = "c304cffd-f331-49a1-a4ba-0706f869de11";
+
+          var modelBindingDefinition = Configuration.Configure<SimpleModelForWeirdnessTest>();
+          var model = modelBindingDefinition.CreateAndBind(new string[] { "/N", nameValue, "/I", idValue });
+          
+          model.Name.Should().Be(nameValue);
+          model.Id.ToString().Should().Be(idValue);
+        }
     }
 }
